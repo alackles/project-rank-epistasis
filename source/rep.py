a8341747e54b0f6d50dec914d0e84d2b3d402d53
Acacia Ackles <alackles@msu.edu>
2021-10-26 13:37:53 -0400

    Rep runner

--- source/rep.py
+++ source/rep.py
@@ -14,8 +14,8 @@ buildpath = "./third-party/MABE2/build/"
 runpath = buildpath + "MABE"
 reppath = "../data/reps/"
 
-genfile = "./third-party/MABE2/settings/NK.gen"
-mabefile = "./third-party/MABE2/settings/NK.mabe"
+mabefile_canon = "./third-party/MABE2/settings/NKRank.mabe"
+mabefile_vari = "./third-party/MABE2/settings/NKVar.mabe"
 
 firstrep = 0
 lastrep = 2
@@ -33,7 +33,7 @@ digs = len(str(lastrep-firstrep))
 os.system("cd " + buildpath + "&& make clean && make && cd ../../../")
 
 for k in kvals:
-      k_var = 'eval_nk.K='+ str(k)
+      k_var = 'eval_nkrank.K='+ str(k)
       for rep in range(firstrep, lastrep):
           randseed = rep
           dirname = reppath + "canon/SEED_" + str(randseed).rjust(digs, '0') + "__K_" + str(k) + "/"
@@ -41,28 +41,29 @@ for k in kvals:
           Path(dirname).mkdir(parents=True, exist_ok=True)
           randseed_var = "random_seed=" + str(randseed)
           outpath_var = 'output.filename=\\"' + dirname + 'output.csv\\"'
-          mutpath_var = 'eval_nk.mutant_file=\\"' + dirname + 'mutants.csv\\"'
-          nkpath_var = 'eval_nk.nk_file=\\"' + dirname + 'nk.csv\\"'
-          genpath_var = 'eval_nk.genome_file=\\"' + dirname + 'ref_genome.csv\\"'
+          mutpath_var = 'eval_nkrank.mutant_file=\\"' + dirname + 'mutants.csv\\"'
+          nkpath_var = 'eval_nkrank.nk_file=\\"' + dirname + 'nk.csv\\"'
+          genpath_var = 'eval_nkrank.genome_file=\\"' + dirname + 'ref_genome.csv\\"'
           settings = k_var + "\;" + randseed_var + "\;" + outpath_var + "\;" + mutpath_var + "\;" + nkpath_var + "\;" + genpath_var
-          os.system(runpath + " -f " + mabefile + " -s " + settings)
+          os.system(runpath + " -f " + mabefile_canon + " -s " + settings)
 
-for kpair in kvals:
+for kpair in kpairs:
     ka = kpair[0]
     kb = kpair[1]
-    ka_var = 'eval_nkmixed.K_a='+ str(ka)
-    kb_var = 'eval_nkmixed.K_b=' + str(kb)
+    ka_var = 'eval_nkvar.K_a='+ str(ka)
+    kb_var = 'eval_nkvar.K_b=' + str(kb)
     for nk in nktypes:
-        nk_var = 'eval_nkmixed.nk_type=' + nk
+        nk_var = 'eval_nkvar.nk_type=\\"' + nk + '\\"'
+        print(nk_var)
         for rep in range(firstrep, lastrep):
-        randseed = rep
-        dirname = reppath + nk + "/SEED_" + str(randseed).rjust(digs, '0') + "__KA_" + str(ka) + "__KB_" + str(kb) +  "/" 
-        print(dirname)
-        Path(dirname).mkdir(parents=True, exist_ok=True)
-        randseed_var = "random_seed=" + str(randseed)
-        outpath_var = 'output.filename=\\"' + dirname + 'output.csv\\"'
-        mutpath_var = 'eval_nkmixed.mutant_file=\\"' + dirname + 'mutants.csv\\"'
-        nkpath_var = 'eval_nkmixed.nk_file=\\"' + dirname + 'nk.csv\\"'
-        genpath_var = 'eval_nkmixed.genome_file=\\"' + dirname + 'ref_genome.csv\\"'
-        settings = ka_var + "\;" + kb_var + "\;" + randseed_var + "\;" + outpath_var + "\;" + mutpath_var + "\;" + nkpath_var + "\;" + genpath_var
-        os.system(runpath + " -f " + mabefile + " -s " + settings)
+            randseed = rep
+            dirname = reppath + nk + "/SEED_" + str(randseed).rjust(digs, '0') + "__KA_" + str(ka) + "__KB_" + str(kb) +  "/" 
+            print(dirname)
+            Path(dirname).mkdir(parents=True, exist_ok=True)
+            randseed_var = "random_seed=" + str(randseed)
+            outpath_var = 'output.filename=\\"' + dirname + 'output.csv\\"'
+            mutpath_var = 'eval_nkvar.mutant_file=\\"' + dirname + 'mutants.csv\\"'
+            nkpath_var = 'eval_nkvar.nk_file=\\"' + dirname + 'nk.csv\\"'
+            genpath_var = 'eval_nkvar.genome_file=\\"' + dirname + 'ref_genome.csv\\"'
+            settings = nk_var + "\;" + ka_var + "\;" + kb_var + "\;" + randseed_var + "\;" + outpath_var + "\;" + mutpath_var + "\;" + nkpath_var + "\;" + genpath_var
+            os.system(runpath + " -f " + mabefile_vari + " -s " + settings)
